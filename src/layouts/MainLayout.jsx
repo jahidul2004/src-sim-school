f50ca970492c77f71ebd3d5fa0b607ca42ec5e31
Jahidul Islam Jihad <islamjahiduljihad@gmail.com>
2025-07-22 21:31:18 +0600

    Footer added.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../shared/NavBar";
+import Footer from "../shared/Footer";
 
 const MainLayout = () => {
     return (
@@ -13,6 +14,7 @@ const MainLayout = () => {
             {/* Outlet end */}
 
             {/* Footer start */}
+            <Footer></Footer>
             {/* Footer end */}
         </>
     );
